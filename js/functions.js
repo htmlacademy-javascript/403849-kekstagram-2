688748802e6604d6d9320679ee0e53e9b0f9a77c
maritana <kozyreva.91@gmail.com>
2025-02-20 19:48:27 +0300

    Задание 5.16: Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -27,3 +27,32 @@ const parseNumber = (string) => {
 };
 
 parseNumber('2025 год');
+
+const validateMeetingTime = (startDay, finishDay, startMeet, durationMeet) => {
+  const numStartDay = startDay.split(':').map((item) => {
+    return parseInt(item);
+  });
+  const numFinishDay = finishDay.split(':').map((item) => {
+    return parseInt(item);
+  });
+  const numStartMeet = startMeet.split(':').map((item) => {
+    return parseInt(item);
+  });
+
+  if (numStartMeet[0] < numStartDay[0]) {
+    return false;
+  }
+
+  if (numStartMeet[0] === numStartDay[0] && numStartMeet[1] < numStartDay[1]) {
+    return false;
+  }
+
+  const numFinishMeet = numStartMeet[0] * 60 + numStartMeet[1] + durationMeet;
+  if (numFinishMeet <= numFinishDay[0] * 60 + numFinishDay[1]) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+validateMeetingTime('8:00', '17:30', '08:00', 900);
