993c4bdc71782060c427dfa0eab557e5ee855525
maritana <kozyreva.91@gmail.com>
2025-02-21 00:40:25 +0300

    Правки по линтеру

--- js/functions.js
+++ js/functions.js
@@ -33,7 +33,7 @@ parseNumber('2025 год');
 const convertHoursToMinutes = (timeString) => {
   const [hours, minutes] = timeString.split(':').map(Number);
   return hours * MINUTES_IN_HOUR + minutes;
-}
+};
 
 const validateMeetingTime = (startDay, finishDay, startMeet, durationMeet) => {
   const numStartDay = convertHoursToMinutes(startDay);
@@ -41,6 +41,6 @@ const validateMeetingTime = (startDay, finishDay, startMeet, durationMeet) => {
   const numStartMeet = convertHoursToMinutes(startMeet);
 
   return numStartDay <= numStartMeet && numStartMeet + durationMeet <= numFinishDay;
-}
+};
 
 validateMeetingTime('8:00', '17:30', '08:00', 900);
